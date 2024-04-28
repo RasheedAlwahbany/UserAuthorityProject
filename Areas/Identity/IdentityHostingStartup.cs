447a2f45a6eac37b280b2089fd1bbc7e9cd24145
RasheedAlwahbany <rasheedalwahbany@gmail.com>
2024-04-28 11:09:18 +0300

    feat: Adding the Razor Login/Register scaffolded items amd config the startup.cs file for run it

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserAuthorityProject.Areas.Identity.Data;
+using UserAuthorityProject.Data;
+
+[assembly: HostingStartup(typeof(UserAuthorityProject.Areas.Identity.IdentityHostingStartup))]
+namespace UserAuthorityProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserAuthDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserAuthDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UserAuthDBContext>();
+            });
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -23,12 +23,14 @@ namespace UserAuthorityProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -51,6 +53,7 @@ namespace UserAuthorityProject
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
