a0e1baa8a77e3b96caa7084b075672e181ac5d2a
RasheedAlwahbany <rasheedalwahbany@gmail.com>
2024-04-28 12:53:08 +0300

    feat: Complete connected to thw sql server and register and account and login to it

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UserAuthorityProject.Areas.Identity.Data;
+using UserAuthorityProject.Data;
 
 namespace UserAuthorityProject
 {
@@ -23,8 +26,18 @@ namespace UserAuthorityProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            var connectionString = Configuration.GetConnectionString("UserAuthDBContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthDbContextConnection' not found.");
+
+            services.AddDbContext<UserAuthDBContext>(options => options.UseSqlServer(connectionString));
+
+            //services.AddDefaultIdentity<ApplicationUser>(options =>
+            //{
+            //    options.SignIn.RequireConfirmedAccount = false;
+            //})
+            //.AddEntityFrameworkStores<UserAuthDBContext>();
+
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
